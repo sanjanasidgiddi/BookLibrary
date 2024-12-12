9704aae1e4c55e632a3f82bd4ae2c48c57ee2407
roland-a <177896797+rolandoarias12@users.noreply.github.com>
2024-12-12 13:17:06 -0500

    Moved changes to their proper folders

--- library/src/main/java/com/revature/library/Service/BookLogService.java
+++ library/src/main/java/com/revature/library/Service/BookLogService.java
@@ -1,5 +1,30 @@
 package com.revature.library.Service;
 
-public class BookLogService {
-    
+import com.revature.library.DAO.BookLogDAO;
+import com.revature.library.Models.BookLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookLogService{
+    private final BookLogDAO dao;
+
+    @Autowired
+    public BookLogService(BookLogDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean isUserHoldingBook(String username){
+        throw new UnsupportedOperationException();
+    }
+
+    public List<BookLog> getAll(){
+        return dao.findAll();
+    }
+
+    public List<BookLog> getAll(String username){
+        throw new UnsupportedOperationException();
+    }
 }
--- library/src/main/java/com/revature/library/Service/UserService.java
+++ library/src/main/java/com/revature/library/Service/UserService.java
@@ -1,15 +1,81 @@
 package com.revature.library.Service;
 
 import com.revature.library.DAO.UserDAO;
+import com.revature.library.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-class UserService{
+public class UserService{
     private final UserDAO dao;
+    private final BookLogService bookLogService;
 
     @Autowired
-    UserService(UserDAO dao) {
+    UserService(UserDAO dao, BookLogService bookLogService) {
         this.dao = dao;
+        this.bookLogService = bookLogService;
+    }
+
+    private boolean validUsername(String username){
+        if (getByUsername(username).isPresent()){
+            return false;
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
+    private boolean validPassword(String username){
+        throw new UnsupportedOperationException();
+    }
+
+    private boolean validFirstOrLastName(String username){
+        throw new UnsupportedOperationException();
+    }
+
+    private boolean validEmail(String username){
+        throw new UnsupportedOperationException();
+    }
+
+    public Optional<User> getByUsername(String username){
+        return dao.findById(username);
+    }
+
+    public List<User> getAll(){
+        return dao.findAll();
+    }
+
+    public Optional<User> addUser(User user){
+        if (!validUsername(user.getUsername())){
+            return Optional.empty();
+        }
+        if (!validPassword(user.getUsername())){
+            return Optional.empty();
+        }
+        if (!validFirstOrLastName(user.getFirstName())){
+            return Optional.empty();
+        }
+        if (!validEmail(user.getEmail())){
+            return Optional.empty();
+        }
+        //TODO check if DOB is valid
+
+        return Optional.of(dao.save(user));
+    }
+
+    //For a user to be deleted, they must exist and not be holding a book
+    public boolean removeUser(String username){
+        if (bookLogService.isUserHoldingBook(username)){
+            return false;
+        }
+
+        if (getByUsername(username).isEmpty()){
+            return false;
+        }
+
+        dao.deleteById(username);
+        return true;
     }
 }
