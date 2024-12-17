084dee0c6b0c29100e6f854d6cc7b4769bf8b697
Sanjana Sidgiddi <sansid247700@gmail.com>
2024-12-16 19:25:32 -0500

    Themes for book-list page added

--- library/src/main/java/com/Frontend/book-list.js
+++ library/src/main/java/com/Frontend/book-list.js
@@ -54,3 +54,15 @@ function closeForm(){
     document.getElementById("id_form_popup").style.display = "none";
 }
 
+var darkl_button = document.getElementById('darklight')
+
+function toggleDarkLight(){
+    /* Toggle Button Text */
+    let current_text = darkl_button.innerText;
+    darkl_button.innerText = current_text === 'Dark' ? 'Light' : 'Dark';
+    /** Log it */
+    console.log("Toggle button clicked!");
+    /** Switch background and text colors using css class under wrapper */
+    var theme_element = document.body;
+    theme_element.classList.toggle("dark_mode");
+}
