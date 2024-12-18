f158829ae36c1faaf854027b49e8158e54a1852e
Sanjana Sidgiddi <sansid247700@gmail.com>
2024-12-18 15:10:39 -0500

    Configured browser router including a link to login page, rendering both navigation and links

--- library/frontend-react/src/App.tsx
+++ library/frontend-react/src/App.tsx
@@ -1,39 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Components/LoginComponent/Login'
 import AllBooks from './Components/BookComponent/allBooks'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Nav from './Components/NavigationLinks/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <> {/*Blank React Fragment*/}
-     {/* <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p> */}
-          <h1>Welcome to the App Page using React</h1> 
-          {/*<AllBooks></AllBooks> */}
+    <>
+      <h1>Welcome to the App Page using React</h1>
+      <BrowserRouter>
+        <Nav></Nav>
+        <Routes>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
+      </BrowserRouter>
+      {/*<AllBooks></AllBooks> */}
     </>
-    
+
   )
 }
 
--- library/frontend-react/src/main.tsx
+++ library/frontend-react/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
+import './index.css'
 import App from './App.tsx'
-import Login from './Components/LoginComponent/Login.tsx'
+// import Login from './Components/LoginComponent/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Login/>
+    <App />
   </StrictMode>,
 )
