25e30a5a012f39767bc3ce213b204c7d5d3117b7
Sanjana Sidgiddi <sansid247700@gmail.com>
2024-12-22 15:43:59 -0500

    Dark mode enabled for LibraryLogs and MyBooks pages

--- library/frontend-react/src/Components/LibraryLogs/LibraryLogsMUI.tsx
+++ library/frontend-react/src/Components/LibraryLogs/LibraryLogsMUI.tsx
@@ -4,63 +4,76 @@ import { useEffect, useState } from "react"
 import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 function LibraryLogs() {
-    const [libraryLogs, setlibraryLogs] = useState<BookLog[]>([])
+  const [libraryLogs, setlibraryLogs] = useState<BookLog[]>([])
 
-    useEffect(() => {
-        // This will execute when the component mounts and on certain other conditions
-        // Send an AXIOS request when the page loads
-        axios.get<BookLog[]>("http://localhost:8080/BookLogs")
-            .then((res) => {
-                setlibraryLogs(res.data)
-            })
-            .catch((error) => {
-                console.error("Error fetching books:", error);
-            });
-    }, [])
+  useEffect(() => {
+    // This will execute when the component mounts and on certain other conditions
+    // Send an AXIOS request when the page loads
+    axios.get<BookLog[]>("http://localhost:8080/BookLogs")
+      .then((res) => {
+        setlibraryLogs(res.data)
+      })
+      .catch((error) => {
+        console.error("Error fetching books:", error);
+      });
+  }, [])
 
-    const formatDate = (date: Date | null) => {
-        if (!date) return "N/A"; // Return N/A if date is missing
-        const formattedDate = new Date(date);
-        return formattedDate.toLocaleDateString("en-US"); 
-    };
+  const formatDate = (date: Date | null) => {
+    if (!date) return "N/A"; // Return N/A if date is missing
+    const formattedDate = new Date(date);
+    return formattedDate.toLocaleDateString("en-US");
+  };
 
-    return (
-        <div>
-            <h1 style={{ marginBottom: '20px', textAlign: 'center' }}>Library Logs</h1>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Username</TableCell>
-                  <TableCell>Book ID</TableCell>
-                  <TableCell>Date Issued</TableCell>
-                  <TableCell>Date to be Returned</TableCell>
-                  <TableCell>Date Actually Returned</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {libraryLogs.length > 0 ? (
-                  libraryLogs.map((log) => (
-                    <TableRow key={log.bookLogId}>
-                      <TableCell>{log.bookLogId}</TableCell>
-                      <TableCell>{log.user.username}</TableCell>
-                      <TableCell>{log.book.bookId}</TableCell>
-                      <TableCell>{formatDate(log.dateIssued)}</TableCell>
-                      <TableCell>{formatDate(log.dateToBeReturned)}</TableCell>
-                      <TableCell>{formatDate(log.dateActuallyReturned)}</TableCell>
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell colSpan={6} style={{ textAlign: 'center' }}>
-                      No logs available.
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-        </div>
-      )
+    /** Function to trigger appearance theme change */
+    let toggleDarkLight = () => {
+      console.log("Dark mode clicked")
+      const darkl_button = document.getElementById('darklight')
+      /* Toggle Button Text */
+      let current_text: string = darkl_button!.innerText;
+      darkl_button!.innerText = current_text === 'Dark' ? 'Light' : 'Dark';
+      /* Switch background and text colors using css class under wrapper */
+      var theme_element = document.body;
+      theme_element.classList.toggle("dark_mode");
+    }
+
+  return (
+    <div>
+      <Button variant="contained" color="secondary" id="darklight" onClick={toggleDarkLight}>Dark</Button>
+      <h1 style={{ marginBottom: '20px', textAlign: 'center' }}>Library Logs</h1>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>Book ID</TableCell>
+            <TableCell>Date Issued</TableCell>
+            <TableCell>Date to be Returned</TableCell>
+            <TableCell>Date Actually Returned</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {libraryLogs.length > 0 ? (
+            libraryLogs.map((log) => (
+              <TableRow key={log.bookLogId}>
+                <TableCell>{log.bookLogId}</TableCell>
+                <TableCell>{log.user.username}</TableCell>
+                <TableCell>{log.book.bookId}</TableCell>
+                <TableCell>{formatDate(log.dateIssued)}</TableCell>
+                <TableCell>{formatDate(log.dateToBeReturned)}</TableCell>
+                <TableCell>{formatDate(log.dateActuallyReturned)}</TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={6} style={{ textAlign: 'center' }}>
+                No logs available.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
 
 export default LibraryLogs
--- library/frontend-react/src/Components/MyBooks/MyBooksMUI.tsx
+++ library/frontend-react/src/Components/MyBooks/MyBooksMUI.tsx
@@ -8,98 +8,100 @@ import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typo
 
 
 function MyBooks() {
-    
-  const [myBooks, setMyBooks] = useState<BookLog[]>([])  
-    
+
+  const [myBooks, setMyBooks] = useState<BookLog[]>([])
+
   useEffect(() => {
-      // This will execute when the component mounts and on certain other conditions
-      // Send an AXIOS request when the page loads
-      axios.get<BookLog[]>("http://localhost:8080/bookLogs")
-          .then((res) => {
-            setMyBooks(res.data)
-          })
-          .catch((error) => {
-                console.error("Error fetching books:", error);
-          });
+    // This will execute when the component mounts and on certain other conditions
+    // Send an AXIOS request when the page loads
+    axios.get<BookLog[]>("http://localhost:8080/bookLogs")
+      .then((res) => {
+        setMyBooks(res.data)
+      })
+      .catch((error) => {
+        console.error("Error fetching books:", error);
+      });
   }, [])
 
   /** Function to trigger appearance theme change */
   let toggleDarkLight = () => {
-      console.log("Dark mode clicked")
-      const darkl_button = document.getElementById('darklight')
-      /* Toggle Button Text */
-      let current_text: string = darkl_button!.innerText;
-      darkl_button!.innerText = current_text === 'Dark' ? 'Light' : 'Dark';
-      /* Switch background and text colors using css class under wrapper */
-      var theme_element = document.body;
-      theme_element.classList.toggle("dark_mode");
+    console.log("Dark mode clicked")
+    const darkl_button = document.getElementById('darklight')
+    /* Toggle Button Text */
+    let current_text: string = darkl_button!.innerText;
+    darkl_button!.innerText = current_text === 'Dark' ? 'Light' : 'Dark';
+    /* Switch background and text colors using css class under wrapper */
+    var theme_element = document.body;
+    theme_element.classList.toggle("dark_mode");
   }
 
   const returnBook = (bookLogId: number) => {
     axios.post(`http://localhost:8080/bookLogs/return/${bookLogId}`)
-        .then(response => {
-            console.log("Book returned:", response.data);
+      .then(response => {
+        console.log("Book returned:", response.data);
 
-        })
-        .catch(error => {
-            console.error("Error returning book:", error);
-            alert("Failed to return the book.");
-        });
+      })
+      .catch(error => {
+        console.error("Error returning book:", error);
+        alert("Failed to return the book.");
+      });
   }
 
   return (
     <div>
-        <div
+      <Button variant="contained" color="secondary" id="darklight" onClick={toggleDarkLight}>Dark</Button>
+      <div
         style={{
-            display:"flex",
-            flexDirection:"row",
-            justifyContent:"space-evenly",
-            gap: "20px", 
-            padding: "10px"
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          gap: "20px",
+          padding: "10px"
         }}>
-      {myBooks.length === 0 ? (
-        <Typography variant="h6">No books borrowed.</Typography>
-      ) : (
-        myBooks.map((log) => {
-            return(
-          <Card key={log.bookLogId} sx={{ maxWidth: 250, marginBottom: 2 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="100"
-                sx={{
-                    width: '100%',
-                    height: 'auto',  
-                    aspectRatio: '5/8', 
-                    objectFit: 'cover', 
-                  }}
-                image={log.book.image}
-                alt="Book cover"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                  {log.book.bookName}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {log.book.bookGenre}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                onClick={() => returnBook(log.book.bookId)}
-              >
-                Return
-              </Button>
-            </CardActions>
-          </Card>
-        )})
-      )}
-    </div></div>
-);
-  
+        {myBooks.length === 0 ? (
+          <Typography variant="h6">No books borrowed.</Typography>
+        ) : (
+          myBooks.map((log) => {
+            return (
+              <Card key={log.bookLogId} sx={{ maxWidth: 250, marginBottom: 2 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="100"
+                    sx={{
+                      width: '100%',
+                      height: 'auto',
+                      aspectRatio: '5/8',
+                      objectFit: 'cover',
+                    }}
+                    image={log.book.image}
+                    alt="Book cover"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {log.book.bookName}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {log.book.bookGenre}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => returnBook(log.book.bookId)}
+                  >
+                    Return
+                  </Button>
+                </CardActions>
+              </Card>
+            )
+          })
+        )}
+      </div></div>
+  );
+
 }
 
 
