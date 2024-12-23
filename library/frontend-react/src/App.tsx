9dcee6f980e857ef601479aa5963c86b2dbc710a
sharvanitamaskar <sharvanitamaskar@gmail.com>
2024-12-22 23:08:33 -0500

    Minor Update

--- library/frontend-react/src/App.tsx
+++ library/frontend-react/src/App.tsx
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div>
-      <h1>Welcome to the Book Library </h1>
+      {/* <h1>Welcome to the Book Library </h1> */}
       <UserInfo.Provider value={
         {
           username,
